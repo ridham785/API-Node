d716216a0265fa7c28c34f95208f4f624baee518
ridham785 <ridham78521@gmail.com>
2022-11-01 02:04:44 +1030

    app.js finished

--- app.js
+++ app.js
@@ -15,5 +15,24 @@ request({url: url},(error, response) =>{
     api.reverse();
    // console.log(api); //reverse the API's
 
+   const myArgs = process.argv.slice(2);
+    
+   var final =[]
+   
+        for(var i=0;i<datas.entries.length;i++)
+        {
+                 
+            if(datas.entries[i].Category == myArgs[0])
+            {
+                final.push(datas.entries[i]);
+
+            }
+        
+        }
+        for(var b=0; b<myArgs[1];b++)
+        {
+            console.log(final[b]);
+        }
+
 
 })
